907b9b1657c399ab2000eb3ab3f937bd6759f80c
eric <eric@onpoint-holdings.com>
2022-02-23 08:08:08 +0800

    home page construction & fontawesome manual integration

--- resources/views/home-page.blade.php
+++ resources/views/home-page.blade.php
@@ -3,77 +3,41 @@
 @section('content')
 <div class="bg-dark text-secondary px-4 py-5 text-center">
     <div class="py-5">
-        <h1 class="display-5 fw-bold text-white">Watch Videos With Friends</h1>
+        <h1 class="display-5 fw-bold text-white"><i class="fa-brands fa-youtube"></i> Watch Videos With Friends</h1>
         <div class="col-lg-6 mx-auto">
         <p class="fs-5 mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum ducimus animi corporis, quaerat quisquam impedit odit recusandae</p>
         <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-            <button type="button" class="btn btn-outline-info btn-md px-4 me-sm-3 fw-bold">Create New Room</button>
-            <button type="button" class="btn btn-outline-light btn-md px-4">Subscribe</button>
+            <button type="button" class="btn btn-outline-info btn-md px-4 me-sm-3 fw-bold"><i class="fa-solid fa-square-plus"></i> Create New Room</button>
+            <button type="button" class="btn btn-outline-light btn-md px-4"><i class="fa-solid fa-heart"></i> Subscribe</button>
         </div>
         </div>
     </div>
 </div>
 
-<div class="container px-4 py-5" id="icon-grid">
-    <h2 class="pb-2 border-bottom">Features Available</h2>
-
-    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4 py-5">
-      <div class="col d-flex align-items-start">
-        <svg class="bi text-muted flex-shrink-0 me-3" width="1.75em" height="1.75em"><use xlink:href="#bootstrap"/></svg>
-        <div>
-          <h4 class="fw-bold mb-0">Featured title</h4>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-        </div>
+<div class="container px-4 py-5 pb-1" id="featured-3">
+  <h2 class="pb-2 border-bottom">Features Available</h2>
+  <div class="row g-4 py-5 row-cols-1 row-cols-lg-3">
+    <div class="feature col">
+      <div class="feature-icon mb-3">
+        <i class="fa-solid fa-comments fa-2xl"></i>
       </div>
-      <div class="col d-flex align-items-start">
-        <svg class="bi text-muted flex-shrink-0 me-3" width="1.75em" height="1.75em"><use xlink:href="#cpu-fill"/></svg>
-        <div>
-          <h4 class="fw-bold mb-0">Featured title</h4>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-        </div>
+      <h2>Chat</h2>
+      <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
+    </div>
+    <div class="feature col">
+      <div class="feature-icon mb-3">
+        <i class="fa-solid fa-video fa-2xl"></i>
       </div>
-      <div class="col d-flex align-items-start">
-        <svg class="bi text-muted flex-shrink-0 me-3" width="1.75em" height="1.75em"><use xlink:href="#calendar3"/></svg>
-        <div>
-          <h4 class="fw-bold mb-0">Featured title</h4>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-        </div>
+      <h2>Video Chat</h2>
+      <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
+    </div>
+    <div class="feature col">
+      <div class="feature-icon mb-3">
+        <i class="fa-solid fa-rotate fa-2xl"></i>
       </div>
-      <div class="col d-flex align-items-start">
-        <svg class="bi text-muted flex-shrink-0 me-3" width="1.75em" height="1.75em"><use xlink:href="#home"/></svg>
-        <div>
-          <h4 class="fw-bold mb-0">Featured title</h4>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-        </div>
-      </div>
-      <div class="col d-flex align-items-start">
-        <svg class="bi text-muted flex-shrink-0 me-3" width="1.75em" height="1.75em"><use xlink:href="#speedometer2"/></svg>
-        <div>
-          <h4 class="fw-bold mb-0">Featured title</h4>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-        </div>
-      </div>
-      <div class="col d-flex align-items-start">
-        <svg class="bi text-muted flex-shrink-0 me-3" width="1.75em" height="1.75em"><use xlink:href="#toggles2"/></svg>
-        <div>
-          <h4 class="fw-bold mb-0">Featured title</h4>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-        </div>
-      </div>
-      <div class="col d-flex align-items-start">
-        <svg class="bi text-muted flex-shrink-0 me-3" width="1.75em" height="1.75em"><use xlink:href="#geo-fill"/></svg>
-        <div>
-          <h4 class="fw-bold mb-0">Featured title</h4>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-        </div>
-      </div>
-      <div class="col d-flex align-items-start">
-        <svg class="bi text-muted flex-shrink-0 me-3" width="1.75em" height="1.75em"><use xlink:href="#tools"/></svg>
-        <div>
-          <h4 class="fw-bold mb-0">Featured title</h4>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-        </div>
-      </div>
+      <h2>Synchronized</h2>
+      <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
     </div>
   </div>
+</div>
 @endsection
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -18,6 +18,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/fontawesome-6/css/all.min.css') }}">
 </head>
 <body>
     <div id="app">
@@ -77,7 +78,7 @@
 
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark" aria-label="Eighth navbar example">
             <div class="container">
-                <a class="navbar-brand" href="{{ route('home') }}">{{ config('app.name') }}</a>
+                <a class="navbar-brand" href="{{ url('/') }}">{{ config('app.name') }}</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample07" aria-controls="navbarsExample07" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -105,8 +106,8 @@
                 </div>
 
                 <div class="text-end">
-                    <button type="button" class="btn btn-sm btn-warning me-2">Sign-up</button>
-                    <button type="button" class="btn btn-sm btn-outline-light">Login</button>
+                    <button type="button" class="btn btn-sm btn-warning me-2" data-bs-target="#signUpModalToggle" data-bs-toggle="modal">Sign-up</button>
+                    <button type="button" class="btn btn-sm btn-outline-light" data-bs-target="#loginModalToggle" data-bs-toggle="modal">Login</button>
                 </div>
             </div>
         </nav>
@@ -114,6 +115,95 @@
         <main>
             @yield('content')
         </main>
+
+        <div class="container">
+            <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+                <div class="col-md-4 d-flex align-items-center">
+                    <a href="{{ url('/') }}" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+                        <i class="fa-brands fa-youtube"></i>
+                    </a>
+                    <span class="text-muted">&copy; {{ config('app.name', 'Watch Party') }}</span>
+                </div>
+
+                <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+                    <li class="ms-3"><a class="text-muted" href="#"><i class="fa-brands fa-facebook"></i></a></li>
+                    <li class="ms-3"><a class="text-muted" href="#"><i class="fa-brands fa-instagram"></i></a></li>
+                    <li class="ms-3"><a class="text-muted" href="#"><i class="fa-brands fa-youtube"></i></a></li>
+                </ul>
+            </footer>
+        </div>
+
+        <div class="modal fade" id="signUpModalToggle" aria-hidden="true" aria-labelledby="signUpModalToggleLabel" tabindex="-1">
+            <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content rounded-5 shadow">
+                    <div class="modal-header p-5 pt-4 pb-4 border-bottom-0">
+                        <h2 class="fw-bold mb-0">Sign up for free</h2>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+
+                    <div class="modal-body p-5 pt-0">
+                        <form class="">
+                            <div class="form-floating mb-3">
+                                <input type="email" class="form-control rounded-4" id="floatingInput" placeholder="name@example.com">
+                                <label for="floatingInput">Email address</label>
+                            </div>
+                            <div class="form-floating mb-3">
+                                <input type="password" class="form-control rounded-4" id="floatingPassword" placeholder="Password">
+                                <label for="floatingPassword">Password</label>
+                            </div>
+                            <button class="w-100 mb-2 btn btn-lg rounded-4 btn-primary" type="submit">Sign up</button>
+                            <small class="text-muted">By clicking Sign up, you agree to the terms of use.</small>
+                            <hr class="my-4">
+                            <h2 class="fs-5 fw-bold mb-3">Or use a third-party</h2>
+                            <button class="w-100 py-2 mb-2 btn btn-outline-danger rounded-4" type="submit">
+                                <i class="fa-brands fa-google"></i>
+                                Sign up with Gmail
+                            </button>
+                            <button class="w-100 py-2 mb-2 btn btn-outline-primary rounded-4" type="submit">
+                                <i class="fa-brands fa-facebook-f"></i>
+                                Sign up with Facebook
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="modal fade" id="loginModalToggle" aria-hidden="true" aria-labelledby="loginModalToggleLabel" tabindex="-1">
+            <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content rounded-5 shadow">
+                    <div class="modal-header p-5 pt-4 pb-4 border-bottom-0">
+                        <h2 class="fw-bold mb-0">Login Account</h2>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+
+                    <div class="modal-body p-5 pt-0">
+                        <form class="">
+                            <div class="form-floating mb-3">
+                                <input type="email" class="form-control rounded-4" id="floatingInput" placeholder="name@example.com">
+                                <label for="floatingInput">Email address</label>
+                            </div>
+                            <div class="form-floating mb-3">
+                                <input type="password" class="form-control rounded-4" id="floatingPassword" placeholder="Password">
+                                <label for="floatingPassword">Password</label>
+                            </div>
+                            <button class="w-100 mb-2 btn btn-lg rounded-4 btn-primary" type="submit">Login</button>
+                            <small class="text-muted">Sign-in account to start your session.</small>
+                            <hr class="my-4">
+                            <h2 class="fs-5 fw-bold mb-3">Or use a third-party</h2>
+                            <button class="w-100 py-2 mb-2 btn btn-outline-danger rounded-4" type="submit">
+                                <i class="fa-brands fa-google"></i>
+                                Login with Gmail
+                            </button>
+                            <button class="w-100 py-2 mb-2 btn btn-outline-primary rounded-4" type="submit">
+                                <i class="fa-brands fa-facebook-f"></i>
+                                Login with Facebook
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </body>
 </html>
